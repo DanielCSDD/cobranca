d7aa8d5bd8340c12a7cb6eab278d3585919917a8
Daniel Coelho <daniel.coelho.santos@gmail.com>
2024-09-17 18:43:20 -0300

    Commit referente a atualização do arquivo app.js, onde foi adicionado a função do modal de exclusão de títulos e uma função para chamar o método @DeleteMaping da classe TituloController.java, que realizará a exclusão no Banco de Dados;

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -29,3 +29,41 @@ $(function () {
         decimal: ','
     });
 });
+
+/* Configurando o modal para exclusão de títulos */
+$('#exclusaoTituloModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+
+    var titulo = button.data('id');
+    var descricao = button.data('descricao');
+    var valor = button.data('valor');
+    var status = button.data('status');
+
+    var modal = $(this);
+
+    modal.find('.modal-title').text('EXCLUIR');
+
+    modal.find('.modal-body span').html('<h6>Tem certeza que deseja EXCLUIR o título?</h6><strong>Descrição:</strong> ' + descricao + ';<br><strong>Valor:</strong> R$ ' + valor + ';<br><strong>Status:</strong> ' + status + ';');
+
+    /* Recuperando o recurso/url do formulário para acessar o método de exclusão */
+    var form = modal.find('form');
+    var action = form.attr('action');
+
+    /* Caso recurso/url não contenha barra (/) será adicionada. */
+    if (!action.endsWith('/')) {
+        action += '/';
+    }
+    onExcluirConfirmado(action, titulo);
+})
+
+/* Função que irá repassar ao mérodo de exclusão o Id do título e o verbo HTTP DELETE. */
+function onExcluirConfirmado(action, titulo) {
+
+    var URL = action + titulo;
+
+    $.ajax({
+        url: URL,
+        method: "DELETE",
+        contentType: 'application/json'
+    });
+}
