9ddbdfa5d555d5575b98550c996e73c1051b03a9
Daniel Coelho <daniel.coelho.santos@gmail.com>
2024-09-16 21:39:05 -0300

    Commit referente a atualização do arquivo TituloController.java, onde foi criado o método editar;

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -8,9 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -20,6 +18,9 @@ import java.util.List;
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
+
+    private static final String VIEW_CADASTRO_TITULO = "CadastroTitulo";
+    private static final String VIEW_PESQUISA_TITULO = "PesquisaTitulos";
 
     private final TituloService tituloService;
     private ModelAndView mv;
@@ -31,7 +32,7 @@ public class TituloController {
 
     @RequestMapping("/novo")
     public ModelAndView novo() {
-        this.mv = new ModelAndView("CadastroTitulo");
+        this.mv = new ModelAndView(VIEW_CADASTRO_TITULO);
         this.mv.addObject(new Titulo());
         return this.mv;
     }
@@ -42,7 +43,7 @@ public class TituloController {
         this.mv = new ModelAndView();
 
         if (errors.hasErrors()) {
-            return "CadastroTitulo";
+            return VIEW_CADASTRO_TITULO;
         }
         this.tituloService.getSalvar(titulo);
         attributes.addFlashAttribute("msgNovoCadastro", "Título cadastrado com sucesso!");
@@ -52,7 +53,7 @@ public class TituloController {
 
     @RequestMapping
     public ModelAndView getPesquisar() {
-        this.mv = new ModelAndView("PesquisaTitulos");
+        this.mv = new ModelAndView(VIEW_PESQUISA_TITULO);
         this.mv.addObject("titulos", this.tituloService.getTitulos());
         return this.mv;
     }
@@ -61,5 +62,12 @@ public class TituloController {
     @ModelAttribute("listaStatusTitulo")
     public List<StatusTitulo> getRelacaoStatus() {
         return Arrays.asList(StatusTitulo.values());
+    }
+
+    @GetMapping("{id}")
+    public ModelAndView editar(@PathVariable("id") Titulo titulo) {
+        this.mv = new ModelAndView(VIEW_CADASTRO_TITULO);
+        this.mv.addObject(titulo);
+        return this.mv;
     }
 }
